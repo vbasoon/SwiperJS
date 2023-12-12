ab5cafb35f6fe3b20cc8f6efd749e4cde92ca465
vbasoon <vbasoon@gmail.com>
2023-12-12 10:19:10 +0200

    first commit

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 var swiper = new Swiper(".mySwiper", {
       direction: 'vertical',
       loop: true,
-      spaceBetween: 10,
       slidesPerView: 4,
       
       
     });
     var swiper2 = new Swiper(".mySwiper2", {
       loop: true,
-      spaceBetween: 10,
       navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
